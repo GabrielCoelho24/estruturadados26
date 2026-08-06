e78ddc2a6659239e211ca96848f3d8d94f541a6f
Gabriel Coelho dos Santos <456621@unoesc.lan>
2026-08-05 22:14:55 -0300

    Palavras Arquivo

---
+++ ATIVIDADES/PALAVRAS/Program.cs
@@ -0,0 +1,28 @@
+﻿string firstName = "Gabriel";
+string lastName = "Santos";
+
+string note = lastName.ToUpper()
+                 + " " + firstName;
+
+string initials = lastName[0] + " " + firstName[0];
+
+//Formatação de strings
+string texto = string.Format(
+    "{0} {1} nascido em {2}",
+    firstName,
+    lastName,
+    "2007"
+    );
+
+Console.WriteLine(texto);
+
+//C# é uma linguagem filha do C++
+// Totalmente Orientada a Objetos
+// Portanto, tudo em C é descendente
+// do tipo object
+
+int age = 19;
+object ageBoxing = age;
+int ageUnboxing = (int)ageBoxing;
+
+//Console.WriteLine(ageUnboxing);
